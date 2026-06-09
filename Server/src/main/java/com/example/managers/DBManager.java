dcf394f10132a2fb90cf59f320f3c99fba07aff2
ledxdd <oVladid11@gmail.com>
2026-06-09 15:09:18 +0300

    DBManager done

--- Server/src/main/java/com/example/managers/DBManager.java
+++ Server/src/main/java/com/example/managers/DBManager.java
@@ -1,26 +1,62 @@
 package com.example.managers;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+
+/**
+ * Базовый класс менеджера базы данных для установки соединения с БД и выполнения запросов к ней
+ *
+ */
 
 public class DBManager {
+    private static DBManager instance;
     final String USER = "s494533";
     final String PASSWD = "eW5IfMpYfQNlAvJ7";
+    final String URL = "jdbc:postgresql://localhost:5433/studs";
     private Connection connection;
     private Statement statement;
 
     public DBManager() throws SQLException {
     }
 
-    public void Connect(String URL) throws SQLException {
-        Connection connection = DriverManager.getConnection(URL, USER, PASSWD);
+    public static DBManager getInstance() throws SQLException {
+        if (instance == null) {
+            instance = new DBManager();
+        }
 
+        return instance;
+    }
+
+    public void connect() throws SQLException {
+        if (connection == null || connection.isClosed()) {
+            connection = DriverManager.getConnection(URL, USER, PASSWD);
+            System.out.println("Connected to bd successfully. \n");
+        }
+    }
+
+    public void disconect() throws SQLException {
+        if (connection != null && !connection.isClosed()) {
+            connection.close();
+            System.out.println("Disconnected from db successfully. \n");
+        }
+    }
+
+    public ResultSet executeQuery(String sql) throws SQLException {
         Statement statement = connection.createStatement();
-        System.out.println("Connected successfully!\n");
-        this.connection = connection;
-        this.statement = statement;
+
+        return statement.executeQuery(sql);
+    }
+
+    public int executeUpdate(String sql) throws SQLException {
+        Statement statement = connection.createStatement();
+
+        return statement.executeUpdate(sql);
+    }
+
+    public PreparedStatement prepareStatement(String sql) throws SQLException {
+        return connection.prepareStatement(sql);
+    }
+
+    public Connection getConnection() {
+        return connection;
     }
 }
