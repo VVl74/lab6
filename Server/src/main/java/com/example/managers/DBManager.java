373c88da7f8f2e49dba0fb247999426ccf425811
ledxdd <oVladid11@gmail.com>
2026-06-22 16:44:10 +0300

    fix

--- Server/src/main/java/com/example/managers/DBManager.java
+++ Server/src/main/java/com/example/managers/DBManager.java
@@ -11,7 +11,7 @@ public class DBManager {
     private static DBManager instance;
     final String USER = "s494533";
     final String PASSWD = "eW5IfMpYfQNlAvJ7";
-    final String URL = "jdbc:postgresql://pg:5432/studs"; // тут нужно будет пробросить порт с твоего компа на сервак, у меня только так оно заработало
+    final String URL = "jdbc:postgresql://pg:5432/studs";
     private Connection connection; // ПОдключение к нашей базе
 
     // Конструктор дефолтный
